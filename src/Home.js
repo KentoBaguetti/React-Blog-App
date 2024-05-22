80d9c06fc2f7a97ba93ba8d17c0831787e09c7bd
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-05-21 22:51:50 -0400

    States as functions

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,48 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
     {
       id: 1,
       title: "First Title",
+      author: "Kentaro",
       body: "This is the body of the first object.",
     },
     {
       id: 2,
       title: "Second Title",
+      author: "Hyogara",
       body: "This is the body of the second object.",
     },
     {
       id: 3,
       title: "Third Title",
+      author: "Kentaro",
       body: "This is the body of the third object.",
     },
     {
       id: 4,
       title: "Fourth Title",
+      author: "Chocochan",
       body: "This is the body of the fourth object.",
     },
     {
       id: 5,
       title: "Fifth Title",
+      author: "Kentaro",
       body: "This is the body of the fifth object.",
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>{blog.body}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 };
