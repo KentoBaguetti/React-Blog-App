ff91ca6bfd109f973c3eb2ffb5d03ab7564eccde
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-05-22 23:22:19 -0400

    Basic Create page - No backend for it yet

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          placeholder="Enter your title here..."
+          required
+          value={title}
+          // e = the event object
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          placeholder="Enter your blog body here..."
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog author:</label>
+        <input
+          type="text"
+          required
+          placeholder="Enter your name here..."
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <button type="submit">Add Blog</button>
+      </form>
     </div>
   );
 };
